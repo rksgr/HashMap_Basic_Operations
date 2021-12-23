f52b8fe307a6e216d0ae949a413be87de53f3612
rksgr <r_kjha@yahoo.com>
2021-12-23 13:23:01 +0530

    Added: Welcome Message

---
+++ MyHashMapMain.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.dec_22_hashmap_test;
+
+        
+public class MyHashMapMain {
+   
+    public static void main(String [] args){
+        System.out.println("Welcome to Bridgelabz!");
+        System.out.println("Today we shall perform operations related to Hashmap");
+    }
+}
